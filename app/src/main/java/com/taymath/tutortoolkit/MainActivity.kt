ecc57dc32a9380b46befe456f038832fa45af1ce
tsb1995 <47466105+tsb1995@users.noreply.github.com>
2020-04-29 23:22:50 -0700

    fix spacing on Choose Icon Fragment

--- app/src/main/java/com/taymath/tutortoolkit/MainActivity.kt
+++ app/src/main/java/com/taymath/tutortoolkit/MainActivity.kt
@@ -2,16 +2,17 @@ package com.taymath.tutortoolkit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 
 /**
  * This main activity is just a container for our fragments,
  * where the real action is.
  */
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/test/java/com/taymath/tutortoolkit/ExampleUnitTest.kt
+++ app/src/test/java/com/taymath/tutortoolkit/ExampleUnitTest.kt
@@ -1,19 +1,8 @@
 package com.taymath.tutortoolkit
 
-import androidx.room.Room
-import androidx.test.platform.app.InstrumentationRegistry
-import com.taymath.tutortoolkit.database.Grade
-import com.taymath.tutortoolkit.database.StudentDatabase
-import com.taymath.tutortoolkit.database.StudentDatabaseDao
-import org.hamcrest.CoreMatchers.equalTo
-import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
-import org.junit.Before
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import java.io.IOException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -24,35 +13,5 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-    }
-}
-
-@RunWith(JUnit4::class)
-class SimpleEntityReadWriteTest {
-    private lateinit var userDao: StudentDatabaseDao
-    private lateinit var db: StudentDatabase
-
-    @Before
-    fun createDb() {
-        val context = InstrumentationRegistry.getInstrumentation().getTargetContext()
-        db = Room.inMemoryDatabaseBuilder(
-            context, StudentDatabase::class.java
-        ).build()
-        userDao = db.studentDatabaseDao
-    }
-
-    @After
-    @Throws(IOException::class)
-    fun closeDb() {
-        db.close()
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun writeUserAndReadInList() {
-        val grade: Grade = Grade()
-        userDao.insertGrade(grade)
-        val byName = userDao.getGrade()
-        assertThat(byName, equalTo(grade))
     }
 }
